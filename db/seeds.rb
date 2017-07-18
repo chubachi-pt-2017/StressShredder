319c72bcc84e123b04d3665c536aff3f12a045aa
prius20 <a1544hm@aiit.ac.jp>
2017-07-18 14:13:30 +0000

    talker data seed add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+talker.create(name:'masuda', pass:'masuda' )
+talker.create(name:'ishida', pass:'ishida' )
