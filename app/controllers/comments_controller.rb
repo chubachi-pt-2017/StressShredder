d300364e7d243853814fc3f730fd5ee4935bce71
prius20 <a1544hm@aiit.ac.jp>
2017-07-18 14:26:36 +0000

    cont. modify

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,38 @@
 class CommentsController < ApplicationController
   def index
+    @msg = 'Comment data. '
+    @data = Comment.all
   end
 
   def add
+    @msg = "edit data.[id = " + params[:id] + "]"
+    @comment = Comment.find(params[:id])
+    if request.patch? then 
+       @comment.update(comment_params)
+     goback
+    end
   end
 
   def edit
+      @msg = "edit data.[id = " + params[:id] + "]"
+    @comment = Comment.find(params[:id])
+    if request.patch? then 
+       @comment.update(comment_params)
+     goback
+    end
   end
 
   def list
+    @msg = 'Comment data. '
+    @data = Comment.all
   end
+
+private
+def comment_params
+   params.require(:comment).permit(:talker_id, :kubun, :comment )
+end
+
+def goback
+  redirect_to '/comments/index'
+end
 end
--- app/controllers/talkers_controller.rb
+++ app/controllers/talkers_controller.rb
@@ -1,13 +1,38 @@
 class TalkersController < ApplicationController
   def index
+    @msg = 'Talker data. '
+    @data = Talker.all
   end
 
   def add
+    @msg = "edit data.[id = " + params[:id] + "]"
+    @talker = Talker.find(params[:id])
+    if request.patch? then 
+       @talker.update(talker_params)
+     goback
+    end
   end
 
   def edit
+    @msg = "edit data.[id = " + params[:id] + "]"
+    @talker = Talker.find(params[:id])
+    if request.patch? then 
+       @talker.update(talker_params)
+     goback
+    end
   end
 
   def list
+    @msg = 'Talker data. '
+    @data = Talker.all
   end
+  
+private
+def talker_params
+   params.require(:talker).permit(:name, :pass)
+end
+
+def goback
+  redirect_to '/talkers/index'
+end
 end
